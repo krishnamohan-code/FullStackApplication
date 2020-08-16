fbee174a034e9833c85bffdbc62b9960381503aa
krishnamohan <krishnamohan.py@gmail.com>
2020-08-16 23:16:45 +0530

    spring boot

---
+++ BookStore/src/main/java/com/model/User.java
@@ -0,0 +1,64 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERTABLE")
+public class User {
+	private Long id;
+	private String name;
+	private String password;
+	private String type;
+	
+	public User() {};
+
+	public User(String name, String password, String type) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.type = type;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "password")
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Column(name = "type")
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
